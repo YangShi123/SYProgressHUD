0c260913d6d16b06f5f05b9c5ed33236a293a687
shiyang <shiyawn@163.com>
2021-08-06 17:39:19 +0800

    - commit

--- SYProgressHUD.podspec
+++ SYProgressHUD.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SYProgressHUD'
-  s.version          = '0.1.0'
+  s.version          = '1.0.1'
   s.summary          = 'A short description of SYProgressHUD.'
 
 # This description is used to generate tags and improve search results.
@@ -21,22 +21,22 @@ Pod::Spec.new do |s|
 TODO: Add long description of the pod here.
                        DESC
 
-  s.homepage         = 'https://github.com/shiyawn@163.com/SYProgressHUD'
+  s.homepage         = 'https://github.com/YangShi123/SYProgressHUD'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
-  s.author           = { 'shiyawn@163.com' => 'shiyawn@163.com' }
-  s.source           = { :git => 'https://github.com/shiyawn@163.com/SYProgressHUD.git', :tag => s.version.to_s }
+  s.author           = { '石头羊子' => 'shiyawn@163.com' }
+  s.source           = { :git => 'https://github.com/YangShi123/SYProgressHUD.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '9.0'
 
   s.source_files = 'SYProgressHUD/Classes/**/*'
   
-  # s.resource_bundles = {
-  #   'SYProgressHUD' => ['SYProgressHUD/Assets/*.png']
-  # }
+   s.resource_bundles = {
+     'SYProgressHUD' => ['SYProgressHUD/Assets/*.png']
+   }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+   s.dependency 'MBProgressHUD'
 end
---
+++ SYProgressHUD/Classes/SYProgressHUD.swift
@@ -0,0 +1,118 @@
+//
+//  SYProgressHUD.swift
+//
+//  基于MBProgressHUD的二次封装
+
+import Foundation
+import MBProgressHUD
+
+public let HUD = SYProgressHUD.shared
+
+public class SYProgressHUD {
+    /// 单例
+    public static let shared = SYProgressHUD()
+    
+    private var hud: MBProgressHUD?
+    
+    /// hud展示时间
+    private var afterDelay: TimeInterval = 2
+    
+    /// hud背景颜色 默认黑色
+    private var backgroundColor: UIColor = .black
+    
+    /// 配置hud属性
+    public func config(afterDelay: TimeInterval? = nil,
+                       backgroundColor: UIColor? = nil) {
+        if afterDelay != nil { self.afterDelay = afterDelay! }
+        if backgroundColor != nil { self.backgroundColor = backgroundColor! }
+    }
+    
+    private func show(_ msg: String,
+                      mode: MBProgressHUDMode? = .text,
+                      view: UIView?,
+                      image: UIImage? = nil,
+                      progress: Float = 0,
+                      autoHidden: Bool = true,
+                      completion: (() -> Void)? = nil) {
+        if hud != nil {
+            hud?.removeFromSuperview()
+            hud = nil
+        }
+        hud = MBProgressHUD.showAdded(to: view ?? UIApplication.shared.windows.last!,
+                                      animated: true)
+        hud?.label.numberOfLines = 0
+        hud?.contentColor = .white
+        hud?.bezelView.style = .solidColor
+        hud?.bezelView.color = self.backgroundColor
+        hud?.animationType = .zoom
+        hud?.margin = 15
+        hud?.removeFromSuperViewOnHide = true
+        hud?.minSize = CGSize(width: 80, height: 50)
+        hud?.label.text = msg
+        hud?.mode = mode!
+        if mode == .annularDeterminate { hud?.progress = progress }
+        if let image = image { hud?.customView = UIImageView(image: image) }
+        if autoHidden { hud?.hide(animated: true, afterDelay: self.afterDelay) }
+        if completion != nil {
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.afterDelay) {
+                completion!()
+            }
+        }
+    }
+    
+    private func hide() {
+        hud?.hide(animated: true)
+    }
+}
+
+extension SYProgressHUD {
+    /// 文字hud
+    public func showMsg(_ msg: String,
+                        view: UIView? = nil,
+                        completion: (() -> Void)? = nil) {
+        show(msg, view: view)
+    }
+    
+    /// 菊花hud
+    public func showIndicator(_ msg: String,
+                              view: UIView? = nil,
+                              completion: (() -> Void)? = nil) {
+        show(msg, mode: .indeterminate, view: view, autoHidden: false, completion: completion)
+    }
+    
+    /// 成功hud
+    public func showSuccess(_ msg: String,
+                            view: UIView? = nil,
+                            completion: (() -> Void)? = nil) {
+        let path = Bundle(for: SYProgressHUD.self).path(forResource: "SYProgressHUD", ofType: "bundle")
+        let bundle = Bundle(path: path!)
+        let file = bundle?.path(forResource: "success@2x", ofType: "png")
+        let image = UIImage(contentsOfFile: file!)?.withRenderingMode(.alwaysOriginal)
+        show(msg, view: view, image: image, completion: completion)
+    }
+    
+    /// 失败hud
+    public func showFailed(_ msg: String,
+                           view: UIView? = nil,
+                           completion: (() -> Void)? = nil) {
+        let path = Bundle(for: SYProgressHUD.self).path(forResource: "SYProgressHUD", ofType: "bundle")
+        let bundle = Bundle(path: path!)
+        let file = bundle?.path(forResource: "fail@2x", ofType: "png")
+        let image = UIImage(contentsOfFile: file!)?.withRenderingMode(.alwaysOriginal)
+        show(msg, view: view, image: image, completion: completion)
+    }
+    
+    /// 进度hud
+    public func showProgress(_ msg: String,
+                             progress: Float,
+                             view: UIView? = nil,
+                             completion: (() -> Void)? = nil) {
+        show(msg, mode: .annularDeterminate, view: view, progress: progress, autoHidden: false, completion: completion)
+    }
+    
+    /// 隐藏hud
+    public func hidden() {
+        hide()
+    }
+}
+
